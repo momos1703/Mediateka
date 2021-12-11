a7a57834b3b57e44b603d76e2d53e2bf79287077
momos1703 <r.momos@gmail.com>
2021-12-11 22:59:55 +0200

    functions to object methods

--- mediateka.js
+++ mediateka.js
@@ -2,40 +2,72 @@
 
 let numberOfFilms;
 
-function start () {
-    numberOfFilms = +prompt ("Скільки фільмів Ви вже переглянули?", "Введіть число");
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt ("Скільки фільмів Ви вже переглянули?", "Введіть число");
-    }
-}
+// function start () {
+//     numberOfFilms = +prompt ("Скільки фільмів Ви вже переглянули?", "Введіть число");
+//     while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+//         numberOfFilms = +prompt ("Скільки фільмів Ви вже переглянули?", "Введіть число");
+//     }
+// }
 
-start();
+// start();
 
 let personalMovieDB = {
     count: numberOfFilms,
     movies: {},
-
     actors: {},
     genres: [],
-    privat: false
+    privat: false,
+    start: function start () {
+            this.count = +prompt ("Скільки фільмів Ви вже переглянули?", "Введіть число");
+            while (this.count == '' || this.count == null || isNaN(this.count)) {
+                this.count = +prompt ("Скільки фільмів Ви вже переглянули?", "Введіть число");
+            }
+        },
+    rememberMyFilms: function rememberMyFilms() {
+        for (let i = 0; i < this.count; i++) {
+    
+            let lastMovie = prompt ("Який фільм Ви переглядали останнім,", "Введіть назву фільму");
+                if (lastMovie !== null && lastMovie !== "Введіть назву фільму" && lastMovie !== '' &&
+                 lastMovie.length <= 50){
+                        let movieMark = prompt ("Як ви оціните цей фільм по 10-бальній системі?", "Поставте оцінку");
+                        if (movieMark !== null && movieMark !== '' && movieMark !== "Поставте оцінку" && 
+                        !isNaN(movieMark)){
+                            this.movies[lastMovie] = +movieMark;
+                        } else {
+                            alert ("Поставте свою оцінку");
+                            i--;
+                        }
+                } else {
+                    alert ("Введені дані не коректні або в них більше 50-ти символів");
+                    i--;
+                }
+        }
+    }  
 };
 
-function rememberMyFilms() {
-    for (let i = 0; i < numberOfFilms; i++) {
+personalMovieDB.start();
+personalMovieDB.rememberMyFilms();
 
-        let lastMovie = prompt ("Який фільм Ви переглядали останнім,", "Введіть назву фільму");
-        let movieMark = prompt ("Як ви оціните цей фільм по 10-бальній системі?", "Поставте оцінку");
-            if (lastMovie !== null && movieMark !== null && lastMovie !== "Введіть назву фільму" && 
-                lastMovie !== '' && movieMark !== '' && lastMovie.length <= 50){
-                personalMovieDB.movies[lastMovie] = +movieMark;
-            } else {
-                alert ("Введені дані не коректні або в них більше 50-ти символів");
-                i--;
-            }
-    }
-}
+console.log(personalMovieDB);
 
-rememberMyFilms();
+
+
+// function rememberMyFilms() {
+//     for (let i = 0; i < numberOfFilms; i++) {
+
+//         let lastMovie = prompt ("Який фільм Ви переглядали останнім,", "Введіть назву фільму");
+//         let movieMark = prompt ("Як ви оціните цей фільм по 10-бальній системі?", "Поставте оцінку");
+//             if (lastMovie !== null && movieMark !== null && lastMovie !== "Введіть назву фільму" && 
+//                 lastMovie !== '' && movieMark !== '' && lastMovie.length <= 50){
+//                 personalMovieDB.movies[lastMovie] = +movieMark;
+//             } else {
+//                 alert ("Введені дані не коректні або в них більше 50-ти символів");
+//                 i--;
+//             }
+//     }
+// }
+
+// rememberMyFilms();
 
 // // let i = 0;
 // // while (i < 2) {
@@ -52,21 +84,21 @@ rememberMyFilms();
 // // }
 // console.log (personalMovieDB);
 
-function detectPersonalLevel() {
-    if (personalMovieDB.count >= 20) {
-        alert ("Оооо, та Ви справжній кіноман)");
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 20){
-        alert ("Ви класичний глядач.");
-    } else if  (personalMovieDB.count < 10 && personalMovieDB.count > 0){
-        alert ("Ви переглянули замало фільмів.");    
-    } else if  (personalMovieDB.count == 0 ||  personalMovieDB.count == null){
-        alert ("Введіть число");  
-    } else {
-        alert ("Сталася помилка, будь ласка, введіть дані повторно.");
-    }
-}
+// function detectPersonalLevel() {
+//     if (personalMovieDB.count >= 20) {
+//         alert ("Оооо, та Ви справжній кіноман)");
+//     } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 20){
+//         alert ("Ви класичний глядач.");
+//     } else if  (personalMovieDB.count < 10 && personalMovieDB.count > 0){
+//         alert ("Ви переглянули замало фільмів.");    
+//     } else if  (personalMovieDB.count == 0 ||  personalMovieDB.count == null){
+//         alert ("Введіть число");  
+//     } else {
+//         alert ("Сталася помилка, будь ласка, введіть дані повторно.");
+//     }
+// }
 
-detectPersonalLevel();
+// detectPersonalLevel();
 
 
 
@@ -77,26 +109,26 @@ detectPersonalLevel();
 // }
 // showMyDB();
 
-function showMyDB(hidden) {
-    if (!hidden){
-        console.log (personalMovieDB);
-    }
-}
+// function showMyDB(hidden) {
+//     if (!hidden){
+//         console.log (personalMovieDB);
+//     }
+// }
 
-showMyDB(personalMovieDB.privat);
+// showMyDB(personalMovieDB.privat);
 
 
-function writeYourGenres(genres) {
-    for (let i = 1; i <= 3; i++){
-        let answers = prompt(`Який Ваш улюблений жанр під номером ${i}?`);
-        if (answers !== '' && answers !== null){
-            genres[i - 1] = answers;
-            //genres[i - 1] = prompt(`Який Ваш улюблений жанр під номером ${i}?`); 
-            //- допустимий такий варіант спрощення, без змінної answers
-        } else {
-            i--;
-        }
-    }
-}
+// function writeYourGenres(genres) {
+//     for (let i = 1; i <= 3; i++){
+//         let answers = prompt(`Який Ваш улюблений жанр під номером ${i}?`);
+//         if (answers !== '' && answers !== null){
+//             genres[i - 1] = answers;
+//             //genres[i - 1] = prompt(`Який Ваш улюблений жанр під номером ${i}?`); 
+//             //- допустимий такий варіант спрощення, без змінної answers
+//         } else {
+//             i--;
+//         }
+//     }
+// }
 
-writeYourGenres(personalMovieDB.genres);
+// writeYourGenres(personalMovieDB.genres);
